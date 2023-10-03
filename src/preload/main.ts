5b0b364543736b0ebf875cfbacc4b9be4bdb4901
Javier Buron <dev@buron.mx>
2023-10-03 09:17:19 -0600

    Fix types in preload main.ts

--- src/preload/main.ts
+++ src/preload/main.ts
@@ -2,11 +2,11 @@ import './bridge'
 
 window.addEventListener('DOMContentLoaded', (): void => {
   const replaceText = (selector: string, text: string): void => {
-    const element: HTMLElement = document.getElementById(selector);
+    const element: HTMLElement | null = document.getElementById(selector);
     if (element) element.innerText = text;
   }
 
-  for (const dependency: string of ['chrome', 'node', 'electron']) {
+  for (const dependency of ['chrome', 'node', 'electron']) {
     const version: string = process.versions[dependency] ?? '';
     replaceText(`${dependency}-version`, version);
   }
